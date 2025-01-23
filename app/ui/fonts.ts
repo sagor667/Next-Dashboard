b4669c01d91f6c99694eaa100bda3cd9f4261efe
Sagor Ahmed <sagorahmed3036@gmail.com>
2025-01-23 08:45:09 +0600

    font subset change

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,8 @@
 import {Style_Script, Rubik_Vinyl} from 'next/font/google';
 
 export const styleScript = Style_Script({
-    weight: '400' 
+    weight: '400'
+    subset: "latin" 
 });
 
 export const rubikVinyl = Rubik_Vinyl({
